36f4607558c16d1380009dff3a6827609efc4f49
Ziya Genc <ziya.genc@datasecure.ai>
2025-04-09 10:12:18 +0200

    Omit empty errors in output report

--- models/models.go
+++ models/models.go
@@ -65,5 +65,5 @@ type ScanResult struct {
 	Version   string       `json:"version"`
 	ScannedAt string       `json:"scannedAt"`
 	Results   LicenseInfos `json:"results"`
-	Errors    TargetErrors `json:"errors,omitempty"`
+	Errors    Errors       `json:"errors,omitempty"`
 }
